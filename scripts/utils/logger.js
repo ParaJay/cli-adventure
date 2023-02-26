7f34e638f706526cb2c9bf250199c7a1eda70551
ParaJay <calumjhunter@gmail.com>
2023-02-26 15:48:41 +0000

    added relog function

--- scripts/utils/logger.js
+++ scripts/utils/logger.js
@@ -5,6 +5,12 @@ function log(message) {
     console.log(message);
 }
 
+function relog() {
+    for(let i = 0; i < logs.length; i++) {
+        console.log(logs[i]);
+    }
+}
+
 module.exports = {
-    logs, log
+    logs, log, relog
 }
