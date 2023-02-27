4108935c93df17d6c4613660dad92cee671e72fa
ParaJay <calumjhunter@gmail.com>
2023-02-27 09:27:00 +0000

    updated advanced journal info test

--- __test__/journal.test.js
+++ __test__/journal.test.js
@@ -1,20 +1,46 @@
 const entities = require("../scripts/entities/entities");
 const {journal} = require("../scripts/journal");
 
+async function testAdvanced(entity) {
+    for(let i = 0; i < 5; i++) {
+        await journal.addEntry(entities.generate(entity));
+    }
+
+    console.log(await journal.getEntry(entity));
+}
+
 describe("test journal functions", () => {
-    test("expect journal.hasEntries to be true", () => {
+    test("expect journal.hasEntries to be true", async () => {
         let entity = entities.random();
 
-        journal.addEntry(entity);
+        await journal.addEntry(entity);
 
         expect(journal.hasEntries()).toBe(true);
     });
-    
-    test("test advanced journal info", () => {
-        for(let i = 0; i < 5; i++) {
-            journal.addEntry(entities.generate("Goblin"));
-        }
+});
 
-        console.log(journal.getEntry("Goblin"));
-    })
+describe("test journal advanced info", () => {
+    test("test goblin info", async() => {
+        await testAdvanced("Goblin");
+    });
+
+    test("test orc info", async() => {
+        await testAdvanced("orc");
+    });
+
+    test("test elf info", async() => {
+        await testAdvanced("elf");
+    });
+
+    test("test wolf info", async() => {
+        await testAdvanced("wolf");
+    });
+
+    test("test lion info", async() => {
+        await testAdvanced("lion");
+    });
+
+    test("test hume info", async() => {
+        await testAdvanced("hume");
+    });
 });
