398d6dbf4f05c6b0a7a766f281e110e074a829c9
ParaJay <calumjhunter@gmail.com>
2023-02-27 19:00:32 +0000

    updated to adhere to item.js

--- scripts/items/food.js
+++ scripts/items/food.js
@@ -1,28 +1,28 @@
 const { Random } = require("../utils/random");
+const items = require("./item");
 
-class Food {
+class Food extends items.Item {
     constructor(name, health) {
-        this.name = name;
+        super(name);
+
         this.health = health;
     }
 }
 
-const berry = new Food("Berry", 5)
+const berry = new Food("Berry", 5);
 const meat = new Food("Meat", 10);
 
-const lookup = {
-    "Berry": berry,
-    "Meat": meat
-}
+items.register(berry);
+items.register(meat);
 
 function getItem(item) {
-    return lookup[item];
+    return items.lookup[item];
 }
 
 function random() {
-    return lookup[new Random().fromArray(Object.keys(lookup))];
+    return items.random("food");
 }
 
 module.exports = {
-    berry, meat, getItem, lookup, random
+    berry, meat, getItem, random
 }
