24a7d9bff27bb59ebeabd5d861686704f24d41a5
ParaJay <calumjhunter@gmail.com>
2023-02-28 22:44:21 +0000

    now registered with item.js

--- scripts/items/armour.js
+++ scripts/items/armour.js
@@ -1,24 +1,35 @@
 const {Random} = require("../utils/random");
 const utils = require("../utils/utils");
-const { Item } = require("./item");
+const items = require("./item");
 const { ranges } = require("../utils/consts");
 
 const armour = ["Helmet", "Chestplate", "Gauntlets", "Greeves", "Boots"];
-class Armour extends Item {
+class Armour extends items.Item {
     constructor(name) {
         super(name);
 
         this.type = utils.capitalize(name);
-        this.rating = ranges.armour[this.name.toLowerCase()].random();
+       
+        this.randomize();
     }
+
+    randomize() { 
+        this.rating = ranges.armour[this.name.toLowerCase()].random();
+        
+        return this;
+     };
 }
 
 function generate(item) {
     return new Armour(item);
 }
 
+for(let i = 0; i < armour.length; i++) {
+    items.register(new Armour(armour[i]));
+}
+
 function random() {
-    return generate(new Random().fromArray(armour));
+    return items.random("armour");
 }
 
 module.exports = {
