93e6e92052df24b2bf8667b289b61bfff51121eb
ParaJay <calumjhunter@gmail.com>
2023-02-28 14:59:58 +0000

    now uses consts.js ranges.armour

--- scripts/items/armour.js
+++ scripts/items/armour.js
@@ -1,24 +1,15 @@
 const {Random} = require("../utils/random");
-const {Range} = require("../utils/range");
 const utils = require("../utils/utils");
 const { Item } = require("./item");
+const { ranges } = require("../utils/consts");
 
 const armour = ["Helmet", "Chestplate", "Gauntlets", "Greeves", "Boots"];
-
-const ranges = {
-    helmet: new Range(4, 9),
-    chestplate: new Range(8, 15),
-    gauntlets: new Range(5, 9),
-    greeves: new Range(6, 10),
-    boots: new Range(5, 7)
-}
-
 class Armour extends Item {
     constructor(name) {
         super(name);
 
         this.type = utils.capitalize(name);
-        this.rating = ranges[this.name.toLowerCase()].random();
+        this.rating = ranges.armour[this.name.toLowerCase()].random();
     }
 }
 
